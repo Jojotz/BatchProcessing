84d0a009df4e9d6f6c79d786836b21457eb7b769
Joan <jcoll1@gmail.com>
2021-07-16 12:35:34 +0200

    First commit

---
+++ src/main/java/batchprocessing/BatchprocessingApplication.java
@@ -0,0 +1,12 @@
+package batchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocessingApplication {
+
+	public static void main(String[] args) throws Exception {
+		    System.exit(SpringApplication.exit(SpringApplication.run(BatchprocessingApplication.class, args)));
+	}
+}
